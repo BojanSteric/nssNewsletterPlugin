1eca84abab4442eb9ab7b5d85ba81e5bd5eb56a7
Bojan Steric <bojansteric7@gmail.com>
2021-03-24 19:03:03 +0100

    initial plugin files

---
+++ nssNewsletterPlugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: NSS Newsletter
+Description: Custom newsletter plugin for NonStopShop eCommerce website
+Version: 1.0.0
+Author: Green Friends
+License: GPLv2 or later
+Text Domain: greenfriends
+*/
