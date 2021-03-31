a9d3825b0c2f60baaf79181183ed93ea229a089f
Nenad <39129200+Icefyre90@users.noreply.github.com>
2021-03-31 23:21:32 +0200

    poravka createdAt postformat

--- Packages/Newsletter/Services/PostFormatter.php
+++ Packages/Newsletter/Services/PostFormatter.php
@@ -7,7 +7,7 @@ class PostFormatter {
 	public static function formatDataNewsForm($postData)
 	{
 		$data['newsStatus'] = (string)$postData['newsStatus'];
-		$data['createdAt'] = $postData['createdAt'] ?? date("Y-m-d H:i:s", new \DateTime() );
+		$data['createdAt'] = $postData['createdAt'] ?? date("Y-m-d H:i:s");
 		$data['scheduledAt'] = (string)$postData['scheduledAt'];
 		$data['title'] = (string)$postData['title'];
 		$data['content'] = (string)$postData['content'];
