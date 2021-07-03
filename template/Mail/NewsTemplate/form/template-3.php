fc6776e668c7151ebb0cc7348b0ed54331b0b784
vjank <v.jankovic992@gmail.com>
2021-07-03 02:51:35 +0200

    feat: new template

---
+++ template/Mail/NewsTemplate/form/template-3.php
@@ -0,0 +1,59 @@
+<span class="contentLabel">Content</span>
+<span class="grayLine"></span>
+<div>
+    <input value="<?=$templateTitle ?? ''?>" class="templateNameForm" name="templateTitle" type="text" placeholder="Unesite naslov">
+    <input value="<?=$templateTitleUrl ?? ''?>" class="templateNameForm" name="templateTitleUrl" type="url" placeholder="Unesite putanju naslova">
+</div>
+<span class="grayLine"></span>
+<div>
+    <?php $inputCounter = $this->getTemplateInput('680x300', $inputCounter, $images)?>
+</div>
+<span class="grayLine"></span>
+<div class="twoItemsNoGrid">
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+</div>
+<div class="twoItemsNoGrid">
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+</div>
+<span class="grayLine"></span>
+<div>
+    <?php $inputCounter = $this->getTemplateInput('680x300', $inputCounter, $images)?>
+</div>
+<span class="grayLine"></span>
+<div>
+    <?php $inputCounter = $this->getTemplateInput('680x300', $inputCounter, $images)?>
+</div>
+<span class="grayLine"></span>
+<div>
+    <?php $inputCounter = $this->getTemplateInput('680x300', $inputCounter, $images)?>
+</div>
+<div class="twoItemsNoGrid">
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+</div>
+<div class="twoItemsNoGrid">
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+    <div>
+        <?php $inputCounter = $this->getTemplateInput('324x500', $inputCounter, $images)?>
+    </div>
+</div>
+<span class="grayLine"></span>
+<div>
+    <?php $inputCounter = $this->getTemplateInput('680x300', $inputCounter, $images)?>
+</div>
